617de8c93cbd45b7380079a66c174d603e65e856
lumenthi <lumenthi@student.42.fr>
2023-01-20 10:23:07 +0100

    :art: :memo: Reboot documentation

--- sources/kernel/kernlib/sources/reboot.c
+++ sources/kernel/kernlib/sources/reboot.c
@@ -1,15 +1,19 @@
+/*
+	The original 8042 "PS/2 keyboard controller" design had some extra pins on the controller and the cheapskate IBM engineers did wire lots of stuff to these extra pins. One pin was wired to the CPU reset pin, another unused pin was used to fix the 80286 address wraparound incompatibility, the fix was called the the A20 gate.
+
+	The command byte 0xFx means "pulse the chosen line down for 6 milliseconds", with E choosing the reset line (the bit 0 is zeroed). The effect is resetting the CPU so that it starts executing code according to its boot sequence.
+*/
+
 #include "kernlib.h"
 
-/* keyboard interface IO port: data and control
-   READ:   status port
-   WRITE:  control register */
-#define KBRD_INTRFC 0x64
+/* Keyboard IO Ports */
+#define KBRD_CMDPORT 0x64
+#define KBRD_DATAPORT 0x60
 
 /* keyboard interface bits */
 #define KBRD_BIT_KDATA 0 /* keyboard data is in buffer (output buffer is empty) (bit 0) */
 #define KBRD_BIT_UDATA 1 /* user data is in buffer (command buffer is empty) (bit 1) */
 
-#define KBRD_IO 0x60 /* keyboard IO port */
 #define KBRD_RESET 0xFE /* reset CPU command */
 
 #define bit(n) (1<<(n)) /* Set bit n to 1 */
@@ -26,13 +30,10 @@ void reboot(void)
 	/* Clear all keyboard buffers (output and command buffers) */
 	do
 	{
-		temp = inb(KBRD_INTRFC); /* empty user data */
+		temp = inb(KBRD_CMDPORT); /* empty user data */
 		if (check_flag(temp, KBRD_BIT_KDATA) != 0)
-			inb(KBRD_IO); /* empty keyboard data */
+			inb(KBRD_DATAPORT); /* empty keyboard data */
 	} while (check_flag(temp, KBRD_BIT_UDATA) != 0);
 
-	outb(KBRD_INTRFC, KBRD_RESET); /* pulse CPU reset line */
-	loop:
-	asm volatile ("hlt"); /* if that didn't work, halt the CPU */
-	goto loop; /* if a NMI is received, halt again */
+	outb(KBRD_CMDPORT, KBRD_RESET); /* pulse CPU reset line */
 }
